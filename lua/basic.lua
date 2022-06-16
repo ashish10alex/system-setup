ad0e021b25febf7c28bbc58f17e7ce1a65cfdc5c
ashish10alex <ashish.alex10@gmail.com>
2022-06-16 02:34:42 +0100

    csharp lsp

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,11 +1,7 @@
 -- print('hello from ~/config/nvim/lua/basic.lua')
-require 'lspconfig'.pyright.setup{
-    on_attach = function()
-        print("Hello from Ashish")
-    end,
-}
+require 'lspconfig'.pyright.setup{}
 require 'lspconfig'.bashls.setup{}
-
 require 'lspconfig'.sumneko_lua.setup{}
+require 'lspconfig'.csharp_ls.setup{}
 
 
