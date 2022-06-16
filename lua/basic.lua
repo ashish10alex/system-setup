67d42295f30ffbd24c0ce8bdc281948030287969
ashish10alex <ashish.alex10@gmail.com>
2022-06-16 02:54:54 +0100

    fix lsps not installing correctly

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,7 +1,13 @@
 -- print('hello from ~/config/nvim/lua/basic.lua')
-require 'lspconfig'.pyright.setup{}
+require("nvim-lsp-installer").setup {}
+require 'lspconfig'.pyright.setup{
+    on_attach = function()
+    vim.keymap.set("n", "gl", vim.lsp.buf.definition, {bufffer=0})
+end,
+}
 require 'lspconfig'.bashls.setup{}
 require 'lspconfig'.sumneko_lua.setup{}
 require 'lspconfig'.csharp_ls.setup{}
+
 
 
