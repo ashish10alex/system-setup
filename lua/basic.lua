fa3590651431cf29f5c6757270e8a61e64500abb
Ashish Alex <ashish.alex10@gmail.com>
2022-10-26 00:09:16 +0100

    add lsp stuff python and diagnostic jumps

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,12 +1,24 @@
 -- print('hello from ~/config/nvim/lua/basic.lua')
 require("nvim-lsp-installer").setup {}
-require 'lspconfig'.pyright.setup{}
+
+
+require 'lspconfig'.pyright.setup{
+  on_attach = function()
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {buffer=0})
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
+  end,
+}
+
 require 'lspconfig'.bashls.setup{}
 require 'lspconfig'.sumneko_lua.setup{}
 require 'lspconfig'.sqlls.setup{}
 -- require 'lspconfig'.csharp_ls.setup{}
 
-require('telescope').load_extension('env')
 --
 -- Add here to change telescope settings
 require('telescope').setup{
@@ -14,7 +26,6 @@ require('telescope').setup{
         prompt_prefix = "> "
     }
 }
-require('telescope').load_extension('fzf')
 
 require'lspconfig'.sumneko_lua.setup {
   settings = {
