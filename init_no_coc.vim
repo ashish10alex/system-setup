20ea24fb69ea0315391f94a61601146442a1a0d5
Ashish Alex <ashish.alex10@gmail.com>
2022-10-26 21:44:58 +0100

    add lsp autocompletion

--- init_no_coc.vim
+++ init_no_coc.vim
@@ -82,6 +82,18 @@ Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 Plug 'williamboman/nvim-lsp-installer'
 Plug 'neovim/nvim-lspconfig'
 
+" Auto completion
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
+
+" For luasnip users.
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }  "shows files, git-file in the current directory, Install bat for syntax highlighting 
 Plug 'junegunn/fzf.vim' 
 
--- lua/basic.lua
+++ lua/basic.lua
@@ -1,8 +1,12 @@
--- print('hello from ~/config/nvim/lua/basic.lua')
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 require("nvim-lsp-installer").setup {}
 
 
 require 'lspconfig'.pyright.setup{
+  capabilities = capabilities,
   on_attach = function()
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
@@ -37,4 +41,88 @@ require'lspconfig'.sumneko_lua.setup {
     },
   },
 }
+
+
+vim.opt.completeopt={"menu", "menuone", "noselect"}
+
+  -- Set up nvim-cmp.
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    completion = {
+              autocomplete = false,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+      -- Use tab and shit tab for autocompletion
+	  ["<Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+		  cmp.select_next_item()
+		elseif vim.fn["vsnip#available"](1) == 1 then
+		  feedkey("<Plug>(vsnip-expand-or-jump)", "")
+		elseif has_words_before() then
+		  cmp.complete()
+		else
+		  fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+		end
+	  end, { "i", "s" }),
+
+	  ["<S-Tab>"] = cmp.mapping(function()
+		if cmp.visible() then
+		  cmp.select_prev_item()
+		elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+		  feedkey("<Plug>(vsnip-jump-prev)", "")
+		end
+	  end, { "i", "s" }),
+
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+       { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  --cmp.setup.cmdline({ '/', '?' }, {
+  --  mapping = cmp.mapping.preset.cmdline(),
+  --  sources = {
+  --    { name = 'buffer' }
+  --  }
+  --})
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  --cmp.setup.cmdline(':', {
+  --  mapping = cmp.mapping.preset.cmdline(),
+  --  sources = cmp.config.sources({
+  --    { name = 'path' }
+  --  }, {
+  --    { name = 'cmdline' }
+  --  })
+  --})
+
 
