2b42f52e27f17bd57c5a94db0913e1b371e22f9d
Ashish Alex <ashish.alex10@gmail.com>
2022-11-05 21:01:24 +0000

    temp fix telescope import issue lua

--- lua/basic.lua
+++ lua/basic.lua
@@ -6,15 +6,12 @@ require 'lspconfig'.sumneko_lua.setup{}
 require 'lspconfig'.sqlls.setup{}
 -- require 'lspconfig'.csharp_ls.setup{}
 
-require('telescope').load_extension('env')
---
 -- Add here to change telescope settings
 require('telescope').setup{
     defaults = {
         prompt_prefix = "> "
     }
 }
-require('telescope').load_extension('fzf')
 
 require'lspconfig'.sumneko_lua.setup {
   settings = {
